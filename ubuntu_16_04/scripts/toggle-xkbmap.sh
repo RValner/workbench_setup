82e64c9588b59643bfabed5ced87254e525876b1
RValner <valnerrobert@gmail.com>
2018-09-10 13:24:28 -0500

    Config bugfixes

--- ubuntu_16_04/scripts/toggle-xkbmap.sh
+++ ubuntu_16_04/scripts/toggle-xkbmap.sh
@@ -8,7 +8,12 @@ CURRENT="$(setxkbmap -query | grep layout | perl -pe 's/^layout: +([^ ]+)$/$1/')
 if [ "$CURRENT" = "us" ] ; then
     setxkbmap -layout "$OTHER_LAYOUT" -option ctrl:nocaps
     notify-send "Layout changed to" "Estonian" -t 1000 --urgency=low
+	sleep 1
+	killall notify-osd
+
 else
     setxkbmap -layout us -option ctrl:nocaps
     notify-send "Layout changed to" "US" -t 1000 --urgency=low
+	sleep 1
+	killall notify-osd
 fi
